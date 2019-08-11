3bee0bc82e559b904e591c537d3a42c3585cf03a
Alan Krajina <alankrajina88@gmail.com>
2019-08-11 11:55:34 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -23,6 +23,14 @@ class SongsController < ApplicationController
     	  redirect_to song_path(@song)
     	end
 
+#      def create
+#    	  @song = Song.new(song_params)
+#    	  @song.save
+#    	  redirect_to song_path(@song)
+#    	end
+
+
+
     	def update
     	  @song = Song.find(params[:id])
     	  @song.update(song_params(:name))
